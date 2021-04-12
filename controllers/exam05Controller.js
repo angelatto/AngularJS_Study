621beb34a70b43f83babec02b61bf03ecba07291
angelatto <pudri109@gmail.com>
2021-04-12 23:29:18 +0900

    using module 'app' and declare controller

---
+++ controllers/exam05Controller.js
@@ -0,0 +1,5 @@
+angular.module("app")
+    .controller("exam05Controller", function($scope){
+        console.log("exam05Controller instanciation (객체 생성 시점 확인)");
+        $scope.controllerName = "exam05Controller";
+    })
