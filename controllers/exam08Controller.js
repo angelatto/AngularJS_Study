c207796c78c8cfe0d0f81a8ae06b9fd5620da904
angelatto <pudri109@gmail.com>
2021-04-13 17:25:30 +0900

    save scope data to rootscope

---
+++ controllers/exam08Controller.js
@@ -0,0 +1,14 @@
+// 메인 모듈인 app을 가져온다. 
+angular.module("app")
+    .controller("exam08Controller", function($scope, $rootScope){
+        console.log("exam08Controller instanciation (객체 생성 시점 확인)");
+        $scope.controllerName = "exam08Controller";
+        
+        $scope.login = (user) => {
+            if(user && user.uid && user.upassword){
+                // 로그인이 성공되었다고 가정 
+                $rootScope.rootUid = user.uid;
+                console.log("rootUid:  " + $rootScope.rootUid);
+            }
+        };
+    });
