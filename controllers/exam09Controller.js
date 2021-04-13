10175f33d2ee13072af76db3c281d4c46810307d
angelatto <pudri109@gmail.com>
2021-04-13 17:51:04 +0900

    (name, message)

--- controllers/exam09Controller.js
+++ controllers/exam09Controller.js
@@ -5,13 +5,14 @@ angular.module("app")
 
         $scope.login = (user) => {
             if(user && user.uid && user.upassword){
-               console.log(user);
-               // 방송하기 
-               $rootScope.$broadcast("loginSuccess", {uid:user.uid});
+               // 방송하기 -> 방송 이름과 방송할 메세지 필요 
+               console.log("exam09 - login()에서 방송 보내기 ");
+               $rootScope.$broadcast("loginSuccess", {uid:user.uid}); 
+               // 방송을 rootScope에서 전달했기 때문에 먼저 루트 부터 방송 수신하고 자신이 방송 수신 
             }
         };
 
-        // 방송 수신 
+        // 방송 수신
         $scope.$on("loginSuccess", (event, message) => {
             console.log("exam09Controller가 loginSuccess 방송 수신함");
             console.log(message);
@@ -19,6 +20,8 @@ angular.module("app")
 
         // logout 방송 전달 
         $scope.logout = () => {
+            // 방송하기 
+            console.log("exam09 - logout()에서 방송 보내기 ");
             $rootScope.$broadcast("logout");
         };
     });
