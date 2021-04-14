de19612abc51a5a5ca8e186e27a7a6397bae2aec
angelatto <pudri109@gmail.com>
2021-04-14 22:41:20 +0900

    exam14Controller- ng-repeat and ng-if

---
+++ controllers/exam14Controller.js
@@ -0,0 +1,12 @@
+// 메인 모듈인 app을 가져온다. 
+angular.module("app")
+    .controller("exam14Controller", function($scope){
+        console.log("exam14Controller instanciation (객체 생성 시점 확인)");
+        $scope.todos = [
+            {action: "동영상 녹화", complete: false},
+            {action: "앵귤러JS 복습", complete: false},
+            {action: "알마인드", complete: false},
+            {action: "밥먹기", complete: false}
+        ];
+    
+    });
